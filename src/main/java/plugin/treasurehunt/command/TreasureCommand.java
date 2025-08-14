0cb1e4f226ce97fb75f2452da67f5f2c91939679
kajiharuhyyy <kajiharu0921@gmail.com>
2025-08-14 23:51:49 +0900

    マルチ対応

--- src/main/java/plugin/treasurehunt/command/TreasureCommand.java
+++ src/main/java/plugin/treasurehunt/command/TreasureCommand.java
@@ -4,25 +4,38 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import plugin.treasurehunt.data.PlayerScore;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.SplittableRandom;
 
 public class TreasureCommand implements CommandExecutor, Listener {
 
-    private Player player;
+    private List<PlayerScore> playerScoreList = new ArrayList<>();
     private Material material;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if(sender instanceof Player player){
-            this.player = player;
+            if (playerScoreList.isEmpty()){
+                addNewPlayer(player);
+            } else {
+                for (PlayerScore playerScore : playerScoreList){
+                    if (!playerScore.getPlayerName().equals(player.getName())){
+                        addNewPlayer(player);
+                    }
+                }
+            }
+
+
             player.setHealth(20);
             player.setFoodLevel(20);
 
@@ -33,17 +46,18 @@ public class TreasureCommand implements CommandExecutor, Listener {
         return false;
     }
 
+
     @EventHandler
     public void onEntityPickupItem(EntityPickupItemEvent e) {
+        if (!(e.getEntity() instanceof Player player)) return;
+        if (playerScoreList.isEmpty() || material == null) return;
         Material picked = e.getItem().getItemStack().getType();
-        if (Objects.isNull(player)){
-            return;
-        }
-        if (Objects.isNull(this.material)){
-            return;
-        }
-        if (this.player.getName().equals(player.getName()) && picked == this.material){
-           player.sendMessage("お宝発見!ゲーム終了!");
+
+        for (PlayerScore playerScore : playerScoreList){
+            if (playerScore.getPlayerName().equals(player.getName()) && picked == this.material) {
+                    playerScore.setScore(playerScore.getScore() + 10);
+                    player.sendMessage("お宝発見!ゲーム終了!" + playerScore.getScore() + "点獲得!");
+            }
         }
     }
 
@@ -55,5 +69,17 @@ public class TreasureCommand implements CommandExecutor, Listener {
         List<Material> materialList = List.of(Material.APPLE,Material.PORKCHOP,Material.EGG);
         int random = new SplittableRandom().nextInt(3);
         return materialList.get(random);
+    }
+
+
+    /**
+     * 新規のプレイヤー情報をリストに追加します。
+     *
+     * @param player　コマンドを実行したプレイヤー
+     */
+    private void addNewPlayer(Player player) {
+        PlayerScore newPlayer = new PlayerScore();
+        newPlayer.setPlayerName(player.getName());
+        playerScoreList.add(newPlayer);
     }
 }
---
+++ src/main/java/plugin/treasurehunt/data/PlayerScore.java
@@ -0,0 +1,16 @@
+package plugin.treasurehunt.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * TreasureHuntのゲームを実行する際のスコア情報を扱うオブジェクト。
+ * プレイヤー名、合計点数、日時などの情報を持つ。
+ */
+@Getter
+@Setter
+public class PlayerScore {
+
+    private String playerName;
+    private int Score;
+}
